af25b8802c859047c67ba0c22c5cdb4e70554f9f
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2022-08-30 21:36:53 +0600

    32

---
+++ js/rest-api-methods.js
@@ -0,0 +1,14 @@
+// CRUD: CREATE, READ, UPDATE, DELETE
+
+// REST API METHODS
+/*
+1. GET: Receive/request information about an API resource. (READ)
+2. POST: Create an API resource. Send data to a server to create the resource. (CREATE)
+3. PUT/PATCH: Update an API resource. (UPDATE)
+4. DELETE: Delete an API resource. (DELETE)
+
+
+PATCH: Used to send data to a server to partially update an existing resource.
+PUT: Create a new resource if does not exist.
+Replace the current resource if it exists.
+*/
